17b003e96dd06eb4784258a085480d46af31a7e0
Richard Shea <rshea@thecubagroup.com>
2014-02-15 23:09:29 +1300

    Initial creation of Models for spellweb and corresponding `south` migration

--- splsplsh_project/spellweb/models.py
+++ splsplsh_project/spellweb/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Teacher(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+    chosen_name = models.CharField(max_length=30)
+    family_name = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['family_name', 'chosen_name']
+
+    def __unicode__(self):
+            return self.user
+
+class Learner(models.Model):
+    teacher = models.ForeignKey(Teacher)
+    chosen_name = models.CharField(max_length=30)
+    family_name = models.CharField(max_length=30)
+    learning_level = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['family_name', 'chosen_name']
+
+class Word(models.Model):
+    level = models.IntegerField()
+    word = models.CharField(primary_key=True, unique=True, max_length=30)
+
+    class Meta:
+        ordering = ['level', 'word']
+
+class Attempt(models.Model):
+    learner = models.ForeignKey(Learner)
+    word = models.ForeignKey(Word)
+    when = models.DateTimeField(auto_now_add=True)
+    success = models.BooleanField()
+
+    class Meta:
+        ordering = ['-when']
+    
