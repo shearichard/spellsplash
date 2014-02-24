eed2c671c166e4f0ad2bb48cfaabf261cd29a826
Richard Shea <rshea@thecubagroup.com>
2014-02-25 01:11:43 +1300

    Changes to core urls.py file to try to make reset on django-registration resolve as it should

--- splsplsh_project/urls.py
+++ splsplsh_project/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+#url(r'^accounts/', include('auth_urls')),
+
 urlpatterns = patterns('',
     url(r'^spellweb/', include('spellweb.urls', namespace="spellweb")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('auth_urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
 )
