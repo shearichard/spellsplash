987666d1b3e142902ebdb9e8bf4286a2c4cf3a18
Richard Shea <rshea@thecubagroup.com>
2014-02-24 21:28:39 +1300

    Make django-trace work ... at last \!

--- splsplsh_project/splsplsh_project/settings/base.py
+++ splsplsh_project/splsplsh_project/settings/base.py
@@ -210,7 +210,7 @@ THIRD_PARTY_APPS = (
     # Database migration helpers:
     'south',
     'registration',
-    'django-trace',
+    'django_trace',
 )
 
 # Apps specific for this project go here.
