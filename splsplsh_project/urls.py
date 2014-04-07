597d138bb2210e4dec7e1cd53fc8b477c20a5aed
Richard Shea <rshea@thecubagroup.com>
2014-04-07 23:11:30 +1200

    A number of changes to try get django-registration working as it should. These attempts have been largely succesful - the only outstanding issue being that 'Reset' redirects to the logon page if you're not logged on ! If you are logged on the 'Reset' works as it should.
    The changes in this commit were motivated by :
    http://stackoverflow.com/questions/15654472/django-template-error-could-not-parse-the-remainder-from-uid
    
    And by :
    
    http://stackoverflow.com/questions/19985103/django-1-6-and-django-registration-built-in-authentication-views-not-picked-up

--- splsplsh_project/urls.py
+++ splsplsh_project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth import views as auth_views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,27 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^spellweb/', include('spellweb.urls', namespace="spellweb")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+      #override the default urls
+      url(r'^accounts/password/change/$',
+                    auth_views.password_change,
+                    name='password_change'),
+      url(r'^accounts/password/change/done/$',
+                    auth_views.password_change_done,
+                    name='password_change_done'),
+      url(r'^accounts/password/reset/$',
+                    auth_views.password_reset,
+                    name='password_reset'),
+      url(r'^accounts/password/reset/done/$',
+                    auth_views.password_reset_done,
+                    name='password_reset_done'),
+      url(r'^accounts/password/reset/complete/$',
+                    auth_views.password_reset_complete,
+                    name='password_reset_complete'),
+      url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+                    auth_views.password_reset_confirm,
+                    name='auth_password_reset_confirm'),
+
+      #and now add the registration urls
+      url(r'^accounts/', include('registration.backends.default.urls')),
+
 )
