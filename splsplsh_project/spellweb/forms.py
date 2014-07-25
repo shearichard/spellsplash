9af6bec638fc4ae705965c6de6891d60a1ef819f
Richard Shea <rshea@thecubagroup.com>
2014-07-25 17:32:09 +1200

    Ripping out the stuff in forms.py and views.py related to the http://kevindias.com/writing/django-class-based-views-multiple-inline-formsets/ to make way for an approach from a different angle

--- splsplsh_project/spellweb/forms.py
+++ splsplsh_project/spellweb/forms.py
@@ -4,9 +4,3 @@ from django.forms.models import inlineformset_factory
 
 from .models import Learner, Word, Attempt 
 
-
-class LearnerTestForm(ModelForm):
-    class Meta:
-        model = Learner
-
-AttemptFormSet = inlineformset_factory(Learner, Attempt)
--- splsplsh_project/spellweb/views.py
+++ splsplsh_project/spellweb/views.py
@@ -6,59 +6,8 @@ from django.utils import timezone
 from django.shortcuts import redirect
 
 from spellweb.models import Learner
-from .forms import AttemptFormSet, LearnerTestForm
 
 from django.views.generic.edit import CreateView
-
-class RecipeCreateView(CreateView):
-    template_name = 'spellweb/attempt_add.html'
-    model = Learner
-    form_class = LearnerTestForm
-    success_url = 'success/'
-
-    def get(self, request, *args, **kwargs):
-        """
-        Handles GET requests and instantiates blank versions of the form
-        and its inline formsets.
-        """
-        self.object = None
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        attempt_form = AttemptFormSet()
-        return self.render_to_response(self.get_context_data(form=form, attempt_form=attempt_form))
-
-    def post(self, request, *args, **kwargs):
-        """
-        Handles POST requests, instantiating a form instance and its inline
-        formsets with the passed POST variables and then checking them for
-        validity.
-        """
-        self.object = None
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        attempt_form = AttemptFormSet(self.request.POST)
-        if (form.is_valid() and attempt_form.is_valid()):
-            return self.form_valid(form, attempt_form)
-        else:
-            return self.form_invalid(form, attempt_form)
-
-    def form_valid(self, form, attempt_form):
-        """
-        Called if all forms are valid. Creates a Recipe instance along with
-        associated Ingredients and Instructions and then redirects to a
-        success page.
-        """
-        self.object = form.save()
-        attempt_form.instance = self.object
-        attempt_form.save()
-        return HttpResponseRedirect(self.get_success_url())
-
-    def form_invalid(self, form, attempt_form):
-        """
-        Called if a form is invalid. Re-renders the context data with the
-        data-filled forms and errors.
-        """
-        return self.render_to_response(self.get_context_data(form=form, attempt_form=attempt_form))
 
 class IndexView(generic.ListView):
     def get(self, request, *args, **kwargs):
