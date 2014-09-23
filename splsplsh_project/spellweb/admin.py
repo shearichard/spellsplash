e11945aaf82574e1f2113a6c330187f2bc515a00
Richard Shea <rshea@thecubagroup.com>
2014-09-23 23:58:17 +1200

    Migration to add the 'Essential Words' data; Also some changes to make the Admin lists a bit prettier

--- splsplsh_project/spellweb/admin.py
+++ splsplsh_project/spellweb/admin.py
@@ -4,9 +4,21 @@ from spellweb.models import Learner
 from spellweb.models import Word    
 from spellweb.models import Attempt 
 
-admin.site.register(Teacher)
-admin.site.register(Learner)
-admin.site.register(Word)
-admin.site.register(Attempt)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('chosen_name', 'family_name')
 
-# Register your models here.
+class LearnerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'learning_level', 'family_name', 'chosen_name')
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('word', 'level', 'source')
+    list_filter = ('source',)
+
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('word', 'when', 'success')
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Learner, LearnerAdmin)
+admin.site.register(Word, WordAdmin)
+admin.site.register(Attempt, AttemptAdmin)
+
