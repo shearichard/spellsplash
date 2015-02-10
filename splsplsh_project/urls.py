4c0119208c21d956bb50417c2db6b52cd5a5f10d
Richard Shea <rshea@thecubagroup.com>
2015-02-11 11:55:53 +1300

    A set of changes intended to resolve static file issues in Heroku

--- splsplsh_project/urls.py
+++ splsplsh_project/urls.py
@@ -1,13 +1,21 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+
+from django.views.static import serve
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 #url(r'^accounts/', include('auth_urls')),
+# Provide static url without leading slash
+_static_url = settings.STATIC_URL
+if _static_url.startswith('/'):
+        _static_url = _static_url[1:]
 
 urlpatterns = patterns('',
+    url(r'^%s(?P<path>.*)$' % _static_url, serve, {'document_root': settings.STATIC_ROOT}),
     url(r'^$', RedirectView.as_view(url='/spellweb/')),
     url(r'^spellweb/', include('spellweb.urls', namespace="spellweb")),
     url(r'^admin/', include(admin.site.urls)),
