642a7e9cd62a509ea826969bade6661f6aefd82e
Richard Shea <rshea@thecubagroup.com>
2015-02-14 23:28:52 +1300

    include /static in STATICFILES_DIR

--- splsplsh_project/splsplsh_project/settings/base.py
+++ splsplsh_project/splsplsh_project/settings/base.py
@@ -97,6 +97,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
+    normpath(join(SITE_ROOT, 'static')),
 )
 
 print "HERO" * 10
