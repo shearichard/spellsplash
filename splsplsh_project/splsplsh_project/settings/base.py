1c8c7f5e3441dcb6cb0189d3be4de3000957e02d
Richard Shea <rshea@thecubagroup.com>
2015-02-14 23:52:52 +1300

    Adjust diagnostics

--- splsplsh_project/splsplsh_project/settings/base.py
+++ splsplsh_project/splsplsh_project/settings/base.py
@@ -103,7 +103,8 @@ STATICFILES_DIRS = (
 print "HERO" * 10
 print 'STATIC_ROOT : ' + STATIC_ROOT
 print 'STATIC_URL : ' + STATIC_URL 
-print 'STATICFILES_DIRS : ' + STATICFILES_DIRS
+print 'STATICFILES_DIRS : ' 
+print STATICFILES_DIRS
 print "HERO" * 10
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
