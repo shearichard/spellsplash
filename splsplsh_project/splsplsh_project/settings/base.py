91ab082631d926b3e8e678d457f920950c93ff62
Richard Shea <rshea@thecubagroup.com>
2015-02-14 23:03:13 +1300

    Static file resolution attempt

--- splsplsh_project/splsplsh_project/settings/base.py
+++ splsplsh_project/splsplsh_project/settings/base.py
@@ -90,7 +90,7 @@ MEDIA_URL = '/media/'
 
 ########## STATIC FILE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
+STATIC_ROOT = normpath(join(SITE_ROOT, 'staticfiles'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
